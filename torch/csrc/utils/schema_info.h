a5aa9ac2196304950e914d309f1f13d60617df86
goldenxuett <ttanx@fb.com>
2022-07-08 16:01:42 -0700

    [JIT] Add argumentValue functions and is_mutable checks to SchemaInfo
    ghstack-source-id: 8e75116d696ffba2f7da275b13a856b8c1afd54b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80972

--- torch/csrc/utils/schema_info.h
+++ torch/csrc/utils/schema_info.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <torch/csrc/jit/frontend/function_schema_parser.h>
+#include <unordered_set>
 
 namespace torch {
 namespace utils {
@@ -15,12 +16,38 @@ namespace utils {
 struct TORCH_API SchemaInfo {
  public:
   explicit SchemaInfo(c10::FunctionSchema schema)
-      : schema_(std::move(schema)) {}
+      : schema_(std::move(schema)), updated_(false) {}
   explicit SchemaInfo(const char* signature)
-      : schema_(torch::jit::parseSchema(signature)) {}
+      : schema_(torch::jit::parseSchema(signature)), updated_(false) {}
+
+  bool is_mutable();
+
+  bool is_mutable(size_t index);
+
+  bool is_mutable(c10::string_view name);
+
+  void addArgumentValue(const std::string& name, const at::IValue& value);
+
+  void addArgumentValues(
+      const std::vector<c10::optional<at::IValue>>& value_list);
+
+  void addArgumentValues(
+      const std::unordered_map<std::string, at::IValue>& values);
 
  private:
+  at::IValue flattenZeroDimIValue(const at::IValue& value) const;
+
+  void generateAliasMaps();
+
+  // Map of argument IValues
+  std::unordered_map<std::string, at::IValue> value_map_;
+
+  // Alias map of inputs with each other
+  std::vector<std::unordered_set<size_t>> input_alias_map_;
+
   c10::FunctionSchema schema_;
+
+  bool updated_;
 };
 } // namespace utils
 } // namespace torch
