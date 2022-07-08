8a80076bdd9f89424f668fea45ac55fc0894ccaa
Catherine Lee <csl@fb.com>
2022-07-08 13:49:11 -0700

    test-reports

--- test/run_test.py
+++ test/run_test.py
@@ -370,7 +370,7 @@ def run_test(
     # Can't call `python -m unittest test_*` here because it doesn't run code
     # in `if __name__ == '__main__': `. So call `python test_*.py` instead.
     argv = [test_module + ".py"] + unittest_args
-    mem_report_path = os.path.join('mem-reports', test_module)
+    mem_report_path = os.path.join('test-reports', 'mem-reports', test_module)
     os.environ["MEM_FILE"] = f"{mem_report_path}/memory_info.log"
     os.makedirs(os.path.join(test_directory, mem_report_path), exist_ok=True)
 
