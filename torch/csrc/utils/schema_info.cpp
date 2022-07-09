17a24912aee56f003088684fc202b8d1058f8e7e
goldenxuett <ttanx@fb.com>
2022-07-08 17:33:09 -0700

    [JIT] Add special cases batch_norm, instance_norm and dropout for SchemaInfo
    ghstack-source-id: f0b296cfbe9d5d3198397427a0f7aa1517ad2be9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81007

--- torch/csrc/utils/schema_info.cpp
+++ torch/csrc/utils/schema_info.cpp
@@ -67,7 +67,26 @@ bool SchemaInfo::is_mutable(size_t index) {
   return std::any_of(
       input_alias_map_[index].begin(),
       input_alias_map_[index].end(),
-      [this](size_t index) { return this->schema_.is_mutable(index); });
+      [this](size_t index) {
+        static const char* batch_norm_literal =
+            "aten::batch_norm(Tensor input, Tensor? weight, Tensor? bias, Tensor? running_mean, Tensor? running_var, bool training, float momentum, float eps, bool cudnn_enabled) -> Tensor";
+        static const char* instance_norm_literal =
+            "aten::instance_norm(Tensor input, Tensor? weight, Tensor? bias, Tensor? running_mean, Tensor? running_var, bool use_input_stats, float momentum, float eps, bool cudnn_enabled) -> Tensor";
+        if (torch::jit::parseSchema(batch_norm_literal) == this->schema_ &&
+            (this->schema_.arguments()[index].name() == "running_mean" ||
+             this->schema_.arguments()[index].name() == "running_var")) {
+          return value_map_.count("training") &&
+              value_map_.at("training").toBool();
+        } else if (
+            torch::jit::parseSchema(instance_norm_literal) == this->schema_ &&
+            (this->schema_.arguments()[index].name() == "running_mean" ||
+             this->schema_.arguments()[index].name() == "running_var")) {
+          return value_map_.count("use_input_stats") &&
+              value_map_.at("use_input_stats").toBool();
+        } else {
+          return this->schema_.is_mutable(index);
+        }
+      });
 }
 
 bool SchemaInfo::is_mutable(c10::string_view name) {
@@ -79,6 +98,13 @@ bool SchemaInfo::is_mutable(c10::string_view name) {
 }
 
 bool SchemaInfo::is_non_deterministic() const {
+  if (torch::jit::parseSchema(
+          "aten::dropout(Tensor input, float p, bool train) -> Tensor") ==
+          this->schema_ &&
+      value_map_.count("train") && !value_map_.at("train").toBool()) {
+    return false;
+  }
+
   static const std::vector<const char*> nondeterministic_ops = {
       "aten::dropout(Tensor input, float p, bool train) -> Tensor",
       "aten::_fused_dropout(Tensor self, float p, Generator? generator) -> (Tensor, Tensor)",
